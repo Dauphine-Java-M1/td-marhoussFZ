78c9a56d88ce2422fd7456cd9bae2b41d8adf3d5
marhfa17 <marhfa17@b028-24.crio.dauphine.fr>
2019-09-30 14:40:43 +0200

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/mf/td00/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.mf.td00;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
